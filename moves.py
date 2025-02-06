f130fdf8ab678ffd4a5598ef0f042ed22d5ff88f
Horse-Lips <CallMeP@outlook.com>
2025-02-06 23:30:07 +0000

    Creating Move class
     - Close #8
     - moves.py
    	- Merged horizontal and vertical move functions into one recursive function
    	- Will need to double check this is still getting moves correctly or if the old functions were wrong as scores have changed

--- moves.py
+++ moves.py
@@ -22,43 +22,10 @@ class Move:
         return self.score > other.score
 
 
-def _get_horizontal(board: np.ndarray, v_index: int, h_index: int, move_list: list[Move], v_offset: int = 1,
-                    h_offset: int = 1) -> None:
+def _get_move(board: np.ndarray, v_index: int, h_index: int,
+              move_list: list[Move], v_offset: int = 1, h_offset: int = 1) -> None:
     """
-        Get moves from coordinate expanding horizontally.
-        Expands vertically when a selection sum < 10.
-    Args:
-        Args:
-        board: Board as a numpy 2D array
-        v_index: Board column index to start from
-        h_index: Board row index to start from
-        move_list: List of moves initially empty
-        v_offset: Board column offset for second index
-        h_offset: Board row offset for second index
-    Returns:
-        None
-    """
-    while h_index + h_offset < board.shape[1]:
-        h_offset += 1
-        result = np.sum(board[v_index:v_index + v_offset, h_index:h_index + h_offset])
-
-        if result == 10:
-            coord_1 = slice(v_index, v_index + v_offset)
-            coord_2 = slice(h_index, h_index + h_offset)
-            score = np.sum(board[coord_1, coord_2] > 0)
-
-            move_list.append(Move(score, (coord_1, coord_2)))
-        elif result < 10 and v_offset == 1:
-            _get_vertical(board, v_index, h_index, move_list, v_offset, h_offset)
-        else:
-            return
-
-
-def _get_vertical(board: np.ndarray, v_index: int, h_index: int, move_list: list[Move], v_offset: int = 1,
-                  h_offset: int = 1) -> None:
-    """
-    Get moves from coordinate expanding vertically.
-    Expands horizontally when a selection sum < 10.
+    Get moves recursively expanding horizontally/vertically from a start point.
     Args:
         board: Board as a numpy 2D array
         v_index: Board column index to start from
@@ -69,20 +36,20 @@ def _get_vertical(board: np.ndarray, v_index: int, h_index: int, move_list: list
     Returns:
         None
     """
-    while v_index + v_offset < board.shape[0]:
-        v_offset += 1
-        result = np.sum(board[v_index:v_index + v_offset, h_index:h_index + h_offset])
+    if v_index + v_offset > board.shape[0] or h_index + h_offset > board.shape[1]:
+        return
 
-        if result == 10:
-            coord_1 = slice(v_index, v_index + v_offset)
-            coord_2 = slice(h_index, h_index + h_offset)
-            score = np.sum(board[coord_1, coord_2] > 0)
+    result = np.sum(board[v_index:v_index + v_offset, h_index:h_index + h_offset])
 
-            move_list.append(Move(score, (coord_1, coord_2)))
-        elif result < 10 and h_index == 1:
-            _get_horizontal(board, v_index, h_index, move_list, v_offset, h_offset)
-        else:
-            return
+    if result == 10:
+        coord_1 = slice(v_index, v_index + v_offset)
+        coord_2 = slice(h_index, h_index + h_offset)
+        score = np.sum(board[coord_1, coord_2] > 0)
+        move_list.append(Move(score, (coord_1, coord_2)))
+
+    elif result < 10:
+        _get_move(board, v_index, h_index, move_list, v_offset + 1, h_offset)
+        _get_move(board, v_index, h_index, move_list, v_offset, h_offset + 1)
 
 
 # Main function to iterate over each element and check all directions
@@ -97,7 +64,6 @@ def get_moves(board: np.ndarray) -> list[Move]:
     move_list = []
     for v_index in range(board.shape[0]):  # Vertical indices
         for h_index in range(board.shape[1]):  # Horizontal indices
-            _get_horizontal(board, v_index, h_index, move_list)
-            _get_vertical(board, v_index, h_index, move_list)
+            _get_move(board, v_index, h_index, move_list)
 
     return move_list
